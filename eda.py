d0cd3fca27fa83f5afcac71e1613535887c6f1b5
danangdb <damar.danang@gmail.com>
2024-06-07 16:42:23 +0800

    first coomit

--- eda.py
+++ eda.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler
 
 # import plotly.express as px
 
